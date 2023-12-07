2ff8b053e1d2d881958c87f1846643bf2dbd8c5c
Alexander221991 <138162632+Alexander221991@users.noreply.github.com>
2023-12-07 16:37:33 +0000

    Deploying to gh-pages from @ sivlvy/JOB-SEEKERS@ddd47e9a096f9b6f76f1291a857d7f933fcf7c9d 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-6995a954.js";console.log("5");
+import"./assets/styles-752214f4.js";import{a as o}from"./assets/vendor-a61d8330.js";o.defaults.baseURL="https://food-boutique.b.goit.study/api";
 //# sourceMappingURL=commonHelpers2.js.map
