8cc0c0fd5c8eb6b855841128ed49fb82cb12bf47
DanilKulak <1998kylak@gmail.com>
2023-12-07 19:58:48 +0200

    nextcommit

--- src/js/home-content/main-products/pagination.js
+++ src/js/home-content/main-products/pagination.js
@@ -1,109 +1,62 @@
+import Pagination from 'tui-pagination';
+
 document.addEventListener('DOMContentLoaded', function () {
-  const body = document.body;
-
-  // Створення елементів контенту і пагінації
-  body.innerHTML += `
-    <div id="content"></div>
-    <div id="pagination"></div>
-  `;
-
-  // Отримання посилань на контейнери
-  const contentContainer = document.getElementById('content');
-  const paginationContainer = document.getElementById('pagination');
+  const contentContainer = document.createElement('div');
+  contentContainer.id = 'content';
+  document.body.appendChild(contentContainer);
 
   const itemsPerPage = 9;
   let currentPage = 1;
   let totalPages = 0;
 
+  const paginationContainer = document.createElement('div');
+  paginationContainer.id = 'pagination';
+  document.body.appendChild(paginationContainer);
+
   function fetchData(page) {
     const apiUrl = `https://food-boutique.b.goit.study/api/products?&page=${page}&limit=${itemsPerPage}`;
 
-    return fetch(apiUrl)
+    fetch(apiUrl)
       .then(response => response.json())
-      .catch(error => {
-        console.error('Error fetching data:', error);
-        throw error; // Перенаправлення помилки для обробки в блоках catch
-      });
+      .then(data => {
+        displayContent(data.results);
+        totalPages = data.totalPages;
+        updatePaginationUI();
+      })
+      .catch(error => console.error('Error fetching data:', error));
   }
 
-  function displayContent(data) {
-    // Перевірка наявності results в об'єкті даних
-    if (data && data.results) {
-      contentContainer.innerHTML = data.results.map(product => `<div>${product.name}</div>`).join('');
-    } else {
-      console.error('Error: Missing "results" property in data:', data);
-    }
+  function displayContent(products) {
+    contentContainer.innerHTML = '';
+
+    products.forEach(product => {
+      const productElement = document.createElement('div');
+      productElement.textContent = product.name;
+      contentContainer.appendChild(productElement);
+    });
   }
 
   function updatePaginationUI() {
-    // Очищення контейнера пагінації
     paginationContainer.innerHTML = '';
 
-    if (totalPages > 1) {
-      // Додавання кнопки "Назад"
-      addButton('Назад', currentPage > 1 ? () => changePage(currentPage - 1) : null, currentPage === 1);
+    const options = {
+      totalItems: totalPages * itemsPerPage,
+      itemsPerPage: itemsPerPage,
+      visiblePages: Math.min(totalPages, 5),
+      centerAlign: true,
+    };
 
-      // Логіка для відображення кнопок сторінок
-      if (totalPages <= 7) {
-        for (let i = 1; i <= totalPages; i++) {
-          addButton(i, () => changePage(i), i === currentPage);
-        }
-      } else {
-        // Логіка для відображення трикрапок і додавання перших і останніх кнопок
-        addButton(1, () => changePage(1), currentPage === 1);
+    const pagination = new Pagination(paginationContainer, options);
 
-        const startPage = Math.max(2, currentPage - 2);
-        const endPage = Math.min(totalPages - 1, currentPage + 2);
-
-        if (startPage > 2) {
-          addButton('...', () => {}, true);
-        }
-
-        for (let i = startPage; i <= endPage; i++) {
-          addButton(i, () => changePage(i), i === currentPage);
-        }
-
-        if (endPage < totalPages - 1) {
-          addButton('...', () => {}, true);
-        }
-
-        addButton(totalPages, () => changePage(totalPages), currentPage === totalPages);
+    pagination.on('beforeMove', event => {
+      const newPage = event.page;
+      if (newPage !== currentPage) {
+        currentPage = newPage;
+        fetchData(currentPage);
       }
-
-      // Додавання кнопки "Вперед"
-      addButton('Вперед', currentPage < totalPages ? () => changePage(currentPage + 1) : null, currentPage === totalPages);
-    }
+    });
   }
 
-  function addButton(text, onClick, isDisabled) {
-    const button = document.createElement('button');
-    button.textContent = text;
-    if (onClick) {
-      button.addEventListener('click', onClick);
-    }
-    button.disabled = isDisabled;
-    paginationContainer.appendChild(button);
-  }
-
-  function changePage(newPage) {
-    if (newPage >= 1) {
-      currentPage = newPage;
-      fetchData(currentPage)
-        .then(data => {
-          displayContent(data);
-          totalPages = data.totalPages || Math.ceil(data.total / itemsPerPage);
-          updatePaginationUI();
-        })
-        .catch(error => console.error('Error changing page:', error));
-    }
-  }
-
-  // Ініціалізація сторінки при завантаженні
-  fetchData(currentPage)
-    .then(data => {
-      displayContent(data);
-      totalPages = data.totalPages || Math.ceil(data.total / itemsPerPage);
-      updatePaginationUI();
-    })
-    .catch(error => console.error('Error initializing page:', error));
+  fetchData(currentPage);
+  updatePaginationUI();
 });
