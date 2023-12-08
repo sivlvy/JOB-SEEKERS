13512d29876d7b55f43430801755b532b08b7f94
ValeriaLes <valeria.l.1994@outlook.com>
2023-12-08 01:46:08 +0100

    added cards;

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,6 @@ import './home-content/home-content.js';
 import './filters/filters.js';
 import './modals/modal-product/modal-product.js';
 import './modals/modal-subscription/modal-subscription.js';
-import './cart-content/cart-content.js'
+
 
 console.log('5');
