08fc43dc1ffdc5d0aaa10a5abafc0c9e37fd5abd
ValeriaLes <valeria.l.1994@outlook.com>
2023-12-08 07:16:39 +0100

    get

--- src/js/home-content/main-products/main-projects.js
+++ src/js/home-content/main-products/main-projects.js
@@ -1,51 +1,62 @@
+import axios from 'axios';
+const cardProduct = document.querySelector('.card-container');
+import { getCurrentProducts } from '../../services/food-api.js';
+console.log(getCurrentProducts)
 
-const cardProduct = document.querySelector('.card-container')
 
-console.log(cardProduct)
 
-const getProductList = async () => {
-	const response = await fetch(
-		'https://food-boutique.b.goit.study/api/products?page=1&limit=6'
-	);
-	const products = await response.json();
-	return products;
-};
+// const getProductList = async () => {
+// 	const response = await fetch(
+// 		'https://food-boutique.b.goit.study/api/products?page=1&limit=6'
+// 	);
+// 	const products = await response.json();
+// 	return products;
+// };
 
-getProductList()
+getCurrentProducts()
 	.then(data => {
 		console.log(data);
 		const products = data.results;
-        cardProduct.insertAdjacentHTML('afterbegin', cardMarkup(products))
-        
-        
+		cardProduct.insertAdjacentHTML('afterbegin', cardMarkup(products));
 	})
 	.catch(error => {
 		console.log(error);
 	});
 
-
-
 function cardMarkup(products) {
 	return products
 		.map(
 			({ img, name, category, size, popularity, price }) =>
-				`<img src="${img}" alt="${name}" loading="lazy" class="product-image"/>
+				`<div class="card-wrapper">
+				<div class="image-wrapper">
+				<img src="${img}" alt="${name}" loading="lazy" class="product-image" width="140" height="140" />
+				</div>
        <div class="product-info">
-         <p>
+         <p class="product-name">
            ${name}
          </p>
-         <p>
-           Category: ${category}
+         <p  class="product-item">
+           Category:<span class="product-more-info"> &nbsp;${category}</span>
          </p>
-         <p>
-           Size: ${size}
+         <p class="product-item">
+           Size:<span class="product-more-info"> &nbsp;${size}</span>
          </p>
-         <p>
-           Populyrity: ${popularity}
+         <p class="product-item">
+           Popularity:<span class="product-more-info"> &nbsp;${popularity}</span>
          </p>
-         <p>
-         ${price}</p>
-       </div>`
+		 
+       </div>
+	   <div class="price-and-add">
+		 <p class="product-price">$${price}</p>
+		 <button class="add-button" type="button">
+		 <svg class="icon-button"width="18" height="18">
+		 <use href="../../../icons.svg#icon-cart-mob" >
+		 </use></svg>
+		 </button>
+
+		 </div>
+	   
+	   </div>`
 		)
 		.join('');
 }
