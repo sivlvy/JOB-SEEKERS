a93890c22651209eab83cdac752879c2384bd3cc
katyalle <katyalekhman@gmail.com>
2023-12-09 23:02:41 +0200

    1

--- src/js/cart-content/cart-content.js
+++ src/js/cart-content/cart-content.js
@@ -1,2 +1,18 @@
 import './cart-products-list/cart-products-list.js';
 import './cart-order/cart-order.js';
+
+
+const cartLink = document.getElementById('cartLink');
+const addButton = document.getElementsByClassName('addButton');
+
+let productsCount = 0;
+
+
+function updateCartLink() {
+  cartLink.textContent = `Cart (${productsCount})`;
+}
+
+addButton.addEventListener('click', function () {
+  productsCount += 1;
+  updateCartLink();
+});
