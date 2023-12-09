d567536dde9a04ee8691a645fe6ea5aee8ec377d
sivlvy <126004145+sivlvy@users.noreply.github.com>
2023-12-09 18:01:15 +0000

    Deploying to gh-pages from @ sivlvy/JOB-SEEKERS@fd7e458939934e406d3ba0eb73d9a92c64a818c7 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/styles-827b5446.js";import{a as l,S as p}from"./assets/vendor-baebd336.js";l.defaults.baseURL="https://food-boutique.b.goit.study/api";const u=async()=>{const{data:t}=await l.get("/products/categories");return t},f=async({value:t,category:e,page:s,limit:o,sortBy:a})=>{const c=new URLSearchParams({page:s,limit:o});t&&c.set("value",t),e&&c.set("category",e),a&&c.set("sortBy",a);const{data:r}=await l.get("/products/",{params:c});return r},g=document.querySelector(".product-list"),n=document.querySelector(".card-container"),h=document.querySelector(".loader");let m="",b="",v=1,d=6;n.offsetWidth>=768&&n.offsetWidth<1440&&(d=8);n.offsetWidth>=1440&&(d=9);f({value:m,category:b,page:v,limit:d}).then(t=>{h.style.visibility="hidden";const e=t.results;g.insertAdjacentHTML("afterbegin",S(e))}).catch(t=>{console.log(t)});function S(t){return t.map(({img:e,name:s,category:o,size:a,popularity:c,price:r})=>`<li class="card-wrapper">
+import"./assets/styles-802a39e0.js";import{a as n,S as u}from"./assets/vendor-baebd336.js";n.defaults.baseURL="https://food-boutique.b.goit.study/api";const g=async()=>{const{data:t}=await n.get("/products/categories");return t},h=async({value:t,category:e,page:s,limit:a,sortBy:o})=>{const r=new URLSearchParams({page:s,limit:a});t&&r.set("value",t),e&&r.set("category",e),o&&r.set("sortBy",o);const{data:c}=await n.get("/products/",{params:r});return c},f=document.querySelector(".product-list"),l=document.querySelector(".card-container"),S=document.querySelector(".loader");let m="",v="",y=1,d=6;l.offsetWidth>=768&&l.offsetWidth<1440&&(d=8);l.offsetWidth>=1440&&(d=9);h({value:m,category:v,page:y,limit:d}).then(t=>{S.style.visibility="hidden";const e=t.results;f.insertAdjacentHTML("afterbegin",b(e))}).catch(t=>{console.log(t)});function b(t){return t.map(({img:e,name:s,category:a,size:o,popularity:r,price:c})=>`<li class="card-wrapper">
 					<div class="image-wrapper">
 					<img src="${e}" alt="${s}" loading="lazy" class="product-image" width="140" height="140" />
 					</div>
@@ -8,18 +8,18 @@ import"./assets/styles-827b5446.js";import{a as l,S as p}from"./assets/vendor-ba
 			 </p>
 			<div class ="product-items">
 			<p  class="product-item">
-			Category:<span class="product-more-info"> &nbsp;${o}</span>
+			Category:<span class="product-more-info"> &nbsp;${a}</span>
 		  </p>
 		  <p class="product-item">
-			Size:<span class="product-more-info"> &nbsp;${a}</span>
+			Size:<span class="product-more-info"> &nbsp;${o}</span>
 		  </p>
 		  <p class="product-item">
-			Popularity:<span class="product-more-info"> &nbsp;${c}</span>
+			Popularity:<span class="product-more-info"> &nbsp;${r}</span>
 		  </p></div>
 			 
 		   </div>
 		   <div class="price-and-add">
-			 <p class="product-price">$${r}</p>
+			 <p class="product-price">$${c}</p>
 			 <button class="add-button" type="button">
 			 <svg class="icon-button"width="18" height="18">
              <use href="../../../icons.svg#icon-cart-mob" >
@@ -28,5 +28,5 @@ import"./assets/styles-827b5446.js";import{a as l,S as p}from"./assets/vendor-ba
 	
 			 </div>
 		   
-		   </li>`).join("")}const i={selectEl:document.querySelector("filterts-categories-select")};u().then(t=>{y(t)}).catch(t=>console.log(t));function y(t){const e='<option disabled selected value="Show All" hidden data-placeholder="true">Categories</option>';i.selectEl.insertAdjacentHTML("afterbegin",e);const s=t.map(o=>`<option value="${o}">${o.replaceAll("_"," ")}</option>`).join("").concat('<option value="">Show All</option>');i.selectEl.insertAdjacentHTML("afterbegin",s),new p({select:i.selectEl,settings:{showSearch:!1,searchHighlight:!0}})}
+		   </li>`).join("")}const i={selectEl:document.querySelector(".filterts-categories-select")};g().then(t=>{E(t)}).catch(t=>console.log(t));function E(t){const e='<option disabled selected value="Show All" hidden data-placeholder="true">Categories</option>';i.selectEl.insertAdjacentHTML("afterbegin",e);const s=t.map(o=>`<option value="${o}">${o.replaceAll("_"," ")}</option>`).join("").concat('<option value="">Show All</option>');i.selectEl.insertAdjacentHTML("beforeend",s),new u({select:i.selectEl,settings:{showSearch:!1,searchHighlight:!0}});const a=new p;console.log(a),i.selectEl.addEventListener("change",o=>a.searchOptionForLocalStorage({key:"category",value:o.target.value}))}class p{constructor(){this.STORAGE_FILTERS_KEY="filters-parameters",this.defaultFilters=new Map([["keyword",null],["category",null],["page",1],["limit",6]]),this.setDefaultFilters(),this.saveFiltersToLocalStorage()}setDefaultFilters(){this.filtersData=JSON.parse(localStorage.getItem(this.STORAGE_FILTERS_KEY))||new Map(this.defaultFilters)}saveFiltersToLocalStorage(){localStorage.setItem(this.STORAGE_FILTERS_KEY,JSON.stringify([...this.filtersData]))}updateFilters({key:e,value:s}){this.filtersData.get(e)!==s&&(this.filtersData.set(e,s),this.saveFiltersToLocalStorage())}searchOptionForLocalStorage({key:e,value:s}){this.updateFilters({key:e,value:s}),console.log(Object.fromEntries([...this.filtersData]))}}console.log(p);
 //# sourceMappingURL=commonHelpers2.js.map
