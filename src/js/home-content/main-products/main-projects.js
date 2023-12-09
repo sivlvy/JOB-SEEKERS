ba08123f80ca0caa0ecac720909dbca2ca600be4
ValeriaLes <valeria.l.1994@outlook.com>
2023-12-09 06:37:45 +0100

    added css for tablet, started with desktop; started change category name

--- src/js/home-content/main-products/main-projects.js
+++ src/js/home-content/main-products/main-projects.js
@@ -3,7 +3,7 @@ import { getCurrentProducts } from '../../services/food-api.js';
 // import { resolvePackageEntry } from 'vite';
 
 
-const cardProduct = document.querySelector('.card-container');
+const cardProduct = document.querySelector('.product-list');
 
 
 let value = ""; 
@@ -29,52 +29,52 @@ getCurrentProducts({value, category, page, limit})
 		console.log(error);
 	});
 
-function cardMarkup(products) {
+	function cardMarkup(products) {
 
-	for(const product of products) {
-		const replaceSymbolsOfCategory = product.category;
-		const replacedCategory = replaceSymbolsOfCategory.replace('_', ' ');
-		console.log(replacedCategory)
-	}
-	
-	return products
-		.map(
-			({ img, name, category, size, popularity, price }) => 
-				// let replacedCategory = category.replace('_', ' ');
+		// products.forEach(product => {
+		// 	category = product.category.replace('_', ' ')
+		// 	console.log(category)
 			
-				`<div class="card-wrapper">
-				<div class="image-wrapper">
-				<img src="${img}" alt="${name}" loading="lazy" class="product-image" width="140" height="140" />
-				</div>
-       <div class="product-info">
-         <p class="product-name">
-           ${name}
-         </p>
-        <div class ="product-items">
-		<p  class="product-item">
-		Category:<span class="product-more-info"> &nbsp;${category}</span>
-	  </p>
-	  <p class="product-item">
-		Size:<span class="product-more-info"> &nbsp;${size}</span>
-	  </p>
-	  <p class="product-item">
-		Popularity:<span class="product-more-info"> &nbsp;${popularity}</span>
-	  </p></div>
-		 
-       </div>
-	   <div class="price-and-add">
-		 <p class="product-price">$${price}</p>
-		 <button class="add-button" type="button">
-		 <svg class="icon-button"width="18" height="18">
-		 <use href="../../../icons.svg#icon-cart-mob" >
-		 </use></svg>
-		 </button>
-
-		 </div>
-	   
-	   </div>`
-			)
-	
-		.join('')
+		// });
 		
-}
+		return products
+			.map(
+				({ img, name, category, size, popularity, price }) => 
+			
+				
+					`<div class="card-wrapper">
+					<div class="image-wrapper">
+					<img src="${img}" alt="${name}" loading="lazy" class="product-image" width="140" height="140" />
+					</div>
+		   <div class="product-info">
+			 <p class="product-name">
+			   ${name}
+			 </p>
+			<div class ="product-items">
+			<p  class="product-item">
+			Category:<span class="product-more-info"> &nbsp;${category}</span>
+		  </p>
+		  <p class="product-item">
+			Size:<span class="product-more-info"> &nbsp;${size}</span>
+		  </p>
+		  <p class="product-item">
+			Popularity:<span class="product-more-info"> &nbsp;${popularity}</span>
+		  </p></div>
+			 
+		   </div>
+		   <div class="price-and-add">
+			 <p class="product-price">$${price}</p>
+			 <button class="add-button" type="button">
+			 <svg class="icon-button"width="18" height="18">
+			 <use href="../../../icons.svg#icon-cart-mob" >
+			 </use></svg>
+			 </button>
+	
+			 </div>
+		   
+		   </div>`
+				)
+		
+			.join('')
+			
+	}
