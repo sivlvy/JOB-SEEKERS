3ff75537108aa61e0d66fe04cfe0815cffcee247
Alexander221991 <kurakoff.sanyaba2@gmail.com>
2023-12-10 14:41:53 +0200

    8

--- src/js/filters/filters.js
+++ src/js/filters/filters.js
@@ -1,9 +1,40 @@
 import { getCategoryList } from '../services/food-api';
+import { getCurrentProducts } from '../services/food-api';
 import SlimSelect from 'slim-select';
 
 const refs = {
 	selectEl: document.querySelector('.filterts-categories-select'),
+	form: document.getElementById('filters-search-form'),
+	input: document.querySelector('.filters-search-input'),
+	btn: document.querySelector('.filters-search-button'),
 };
+
+// getCurrentProducts()
+// 	.then(data => renderProductsForValue(data))
+// 	.catch(err => console.log(err));
+
+async function renderProductsForValue(event) {
+	event.preventDefault();
+	const searchQuery = refs.input.value;
+
+	try {
+		const data = await getCurrentProducts({
+			value: searchQuery,
+			category: refs.selectEl.value,
+			page: 1,
+			limit: 6,
+			sortBy: 'relevant',
+		});
+
+		console.log(data);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+refs.form.addEventListener('submit', renderProductsForValue);
+
+// *********************************************\\
 
 getCategoryList()
 	.then(data => {
