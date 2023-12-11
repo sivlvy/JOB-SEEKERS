a8a45226a270bd366133ea3dab1d9b865dadcd97
ValeriaLes <valeria.l.1994@outlook.com>
2023-12-11 04:45:37 +0100

    removeEventListener after click

--- src/js/home-content/main-products/main-projects.js
+++ src/js/home-content/main-products/main-projects.js
@@ -24,7 +24,12 @@ getCurrentProducts(newFilters)
 
 		for (const addButton of addButtons) {
 			addButton.addEventListener('click', onAddButtonClick);
+			
+		
+
 		}
+
+
 	})
 	.catch(error => {
 		console.log(error);
@@ -61,8 +66,9 @@ function cardMarkup(products) {
 			 <p class="product-price">$${price}</p>
 			 <button class="add-button" type="button" data-id="${_id}">
 			 <svg class="icon-button"width="18" height="18">
-             <use href="/icons.svg#icon-cart-mob" >
-             </use></svg>
+             <use href="/icons.svg#icon-cart-mob" class="svg-change">
+             </use>
+			 </svg>
 			 </button>
 	
 			 </div>
@@ -74,16 +80,110 @@ function cardMarkup(products) {
 }
 
 async function onAddButtonClick(event) {
+	
+	
+	
 	const productID = event.currentTarget.dataset.id;
+
 	const savedProducts = JSON.parse(localStorage.getItem('products')) || [];
+
+	
+
+	
 
 	try {
 		const product = await getProductById(productID);
+		console.log(product)
 
 		savedProducts.push(product);
 
+		
+		basketProductMarkUp(product)
+
 		localStorage.setItem('products', JSON.stringify(savedProducts));
+
+		const addButtons = document.querySelectorAll('.add-button');
+		
+
+		for (const addButton of addButtons) {
+
+			const buttonID = addButton.dataset.id;
+		
+			
+
+
+			
+			
+			if(buttonID === productID) {
+				
+			
+				addButton.removeEventListener('click', onAddButtonClick);
+				console.log(addButton.children)
+				const svg = document.querySelectorAll('.svg-change')
+				console.log(svg)
+				// svg.setAttribute("href", "./icons.svg#icon-cart-success");
+			}
+			
+		
+
+		}
+
+		
+		
+	
+
+		
+
+	
+		
+		
+
+
+		
 	} catch (err) {
 		throw new Error(err);
 	}
+
+}
+
+function basketProductMarkUp({
+    img,
+    name,
+    category,
+    size,
+    popularity,
+    _id,
+    price,
+}) {
+    return `<li class="card-wrapper" data-id="${_id}">
+            <div class="image-wrapper">
+            <img src="${img}" alt="${name}" loading="lazy" class="product-image" width="140" height="140" />
+            </div>
+            <div class="product-info">
+            <p class="product-name">
+            ${name}
+            </p>
+            <div class ="product-items">
+            <p  class="product-item">
+            Category:<span class="product-more-info"> &nbsp;${category.replaceAll('_',' ')}</span>
+            </p>
+            <p class="product-item">
+            Size:<span class="product-more-info"> &nbsp;${size}</span>
+            </p>
+            <p class="product-item">
+            Popularity:<span class="product-more-info"> &nbsp;${popularity}</span>
+            </p></div>
+            
+            </div>
+            <div class="price-and-add">
+            <p class="product-price">$${price}</p>
+            <button class="add-button" type="button" data-id="${_id}">
+            <svg class="icon-button"width="18" height="18">
+            <use href="/icons.svg#icon-cart-mob" >
+            </use></svg>
+            </button>
+            
+            </div>
+            
+            </li>`
 }
