76b55c1a9cce2b39218cf1bec629cf71a8643a27
Elena Melnyk <100163385+elenamel1803@users.noreply.github.com>
2023-12-12 23:28:28 +0200

    fixed pagination and filters

--- src/js/filters/filters.js
+++ src/js/filters/filters.js
@@ -1,14 +1,16 @@
 import { getCategoryList } from '../services/food-api';
 import { cardMarkup } from '../home-content/main-products/main-projects';
 import { getCurrentProducts } from '../services/food-api';
-import { saveToLS } from '../services/helpers';
+import { saveToLS, loadFromLS } from '../services/helpers';
 import SlimSelect from 'slim-select';
 
 const refs = {
 	selectEl: document.querySelector('.filters-categories-select'),
 	cardProduct: document.querySelector('.product-list'),
 	formEl: document.querySelector('.filters-form'),
+	test: document.querySelector('.cart__products .js-cart-products'),
 };
+console.log(refs);
 
 export let filters = {
 	keyword: '',
@@ -48,46 +50,48 @@ function renderSelectList(data) {
 	});
 }
 
-refs.formEl.addEventListener('submit', onSubmit);
+// refs.formEl.addEventListener('submit', onSubmit);
 
-function onSubmit(evt) {
-	evt.preventDefault();
-	filters.page = 1;
-	filters.keyword = evt.currentTarget.elements.searchQuery.value
-		.trim()
-		.toLowerCase()
-		.split(' ')
-		.join(' ');
-	saveToLS('filters-parameters', filters);
-	renderProductList();
-}
+// function onSubmit(evt) {
+// 	evt.preventDefault();
+// 	filters.page = 1;
+// 	filters.keyword = evt.currentTarget.elements.searchQuery.value
+// 		.trim()
+// 		.toLowerCase()
+// 		.split(' ')
+// 		.join(' ');
+// 	saveToLS('filters-parameters', filters);
+// 	renderProductList();
+// }
 
-refs.selectEl.addEventListener('change', onSelect);
+// refs.selectEl.addEventListener('change', onSelect);
 
-function onSelect(evt) {
-	filters.category = evt.target.value;
-	filters.page = 1;
-	saveToLS('filters-parameters', filters);
-	renderProductList();
-}
+// function onSelect(evt) {
+// 	filters.category = evt.target.value;
+// 	filters.page = 1;
+// 	saveToLS('filters-parameters', filters);
+// 	renderProductList();
+// }
 
-export async function renderProductList() {
-	try {
-		const data = await getCurrentProducts(filters);
-		refs.cardProduct.innerHTML = cardMarkup(data.results);
-	} catch (err) {
-		console.log(err);
-	}
-}
+// export async function renderProductList() {
+// 	const dataFromLS = loadFromLS('filters-parameters');
+// 	console.log(dataFromLS);
+// 	try {
+// 		const data = await getCurrentProducts(dataFromLS);
+// 		refs.cardProduct.innerHTML = cardMarkup(data.results);
+// 	} catch (err) {
+// 		console.log(err);
+// 	}
+// }
 
 export function changingLimit() {
 	if (window.innerWidth >= 1440) {
-	  filters.limit = 9;
+		filters.limit = 9;
 	} else if (window.innerWidth >= 768) {
-	  filters.limit = 8;
+		filters.limit = 8;
 	} else {
-	  filters.limit = 6;
+		filters.limit = 6;
 	}
-  
+
 	return filters.limit;
-  }
+}
