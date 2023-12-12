b6eafcba35c8bdff82b1a10e2f2c67d4ad47102d
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2023-12-12 04:46:13 -0500

    updated
    modal menu picture for media, closeBtn only

--- src/js/home-content/discount-products/discount-products.js
+++ src/js/home-content/discount-products/discount-products.js
@@ -1,36 +1,20 @@
 import { getDiscountProducts } from '../../services/food-api';
-
+import { clickproduct } from '../../modals/modal-product/modal-product';
 const discount = document.querySelector('.discount-list');
-const paramDiscount = {
-	position: 'top-right',
-	timeout: 3000,
-	width: '400px',
-	fontSize: '30px',
-};
 
 discount.innerHTML = '';
-getDiscountProducts()
-	.then(data => {
-		discount.innerHTML = createMarkup(data);
-	})
-	.catch(onSearchError);
+getDiscountProducts().then(data => {
+	discount.innerHTML = createMarkup(data);
+});
 
 document.addEventListener('click', function (event) {
 	const discountCard = event.target.closest('.discount-card');
 	if (discountCard) {
 		const productId = discountCard.dataset.productId;
-		// productId for call modal window
-		// window.location.href = `/products/${productId}`;
-		console.log('ID:', productId);
+		// console.log('ID:', productId);
+		clickproduct(productId);
 	}
 });
-
-function onSearchError() {
-	Notiflix.Notify.failure(
-		'Something went wrong! Please try again.',
-		paramDiscount
-	);
-}
 
 function getRandomElements(array, count) {
 	const shuffledArray = array.slice().sort(() => Math.random() - 0.5);
@@ -55,23 +39,24 @@ function createMarkup(array) {
 				return `<li class="discount-card" data-product-id="${_id}">
             <div class="discount-img-wrap">
 			  <div class="discount-top-section">
-               <a class="discount-gallery-link" href="#">
+               <a class="discount-gallery-link" >
                 <img src="${img}" alt="${name}" width="114" height="114" loading="lazy" />
                </a>
 			  </div>
 			  <div class="discount-product">
-              	<p class="discount-info-item">${name}</p>
+              	<p class="discount-info-item">${name.substring(0, 19)}</p>
 				<div class="discount-prisce-button">
-             	<p class="discount-price-item">${price}</p>
-             	<button class="add-cart-button add-button" data-id="${_id}>
-					<svg class="discount-icon-button" width="18" height="18">
-		 			<use href="../../../icons.svg#icon-cart-mob" alt="cart" >
-		 			</use></svg>
+             	<p class="discount-price-item">$${price}</p>
+             	<button class="add-discount-button add-button" type="button" data-id="${_id}>
+					<svg class="discount-icon-button bagbag" width="18" height="18">
+		 			<use href="../../../icons.svg#cart" >
+		 			</use>
+					</svg>
 				</button>
 				</div>
 			  </div>
 				<svg class="discount-icon" width="60" height="60">
-                <use href="../../../icons.svg#icon-discount" alt="Discount"></use>
+                <use href="/icons.svg#icon-discount" alt="Discount"></use>
                 </svg>
             </div>
           </li>`;
--- src/js/home-content/popular-products/popular-products.js
+++ src/js/home-content/popular-products/popular-products.js
@@ -1,27 +1,19 @@
 import { getPopularProducts } from '../../services/food-api';
-
+import { clickproduct } from '../../modals/modal-product/modal-product';
 const popular = document.querySelector('.popular-list');
-const paramDiscount = {
-	position: 'top-right',
-	timeout: 3000,
-	width: '400px',
-	fontSize: '30px',
-};
+
 let limit = 5;
 popular.innerHTML = '';
-getPopularProducts(limit)
-	.then(data => {
-		popular.innerHTML = createMarkup(data);
-	})
-	.catch(onSearchError);
+getPopularProducts(limit).then(data => {
+	popular.innerHTML = createMarkup(data);
+});
 
 document.addEventListener('click', function (event) {
-	const popularCard = event.target.closest('.popular-list');
+	const popularCard = event.target.closest('.popular-card');
 	if (popularCard) {
 		const productId = popularCard.dataset.productId;
-		// productId for call modal window
-		// window.location.href = `/products/${productId}`;
-		console.log('ID:', productId);
+		// console.log('ID:', productId);
+		clickproduct(productId);
 	}
 });
 
@@ -42,14 +34,14 @@ function createMarkup(array) {
 				return `<li class="popular-card" data-product-id="${_id}">
             <div class="popular-img-wrap">
                 <div class="popular-left-section">
-                    <a class="popular-gallery-link" href="#">
+                    <a class="popular-gallery-link" >
                         <img src="${img}" alt="${name}" width="56" height="56" loading="lazy" />
                     </a>
                 </div>
                 <div class="popular-center-section">
 					<div class="center-section-up">
-                		<p class="popular-info-item">${name}</p>
-						<button class="add-popular-button add-button" data-id="${_id}">
+                		<p class="popular-info-item">${name.substring(0, 15)}</p>
+						<button class="add-popular-button add-button" type="button" data-id="${_id}">
 							<svg class="popular-icon-button" width="12" height="12">
 		 					<use href="../../../icons.svg#icon-cart-mob" >
 		 					</use>
@@ -69,13 +61,4 @@ function createMarkup(array) {
 			}
 		)
 		.join('');
-}
-{
-	/* <p class="popular-category-item">Category:${category}</p>; */
-}
-function onSearchError() {
-	Notiflix.Notify.failure(
-		'Something went wrong! Please try again.',
-		paramDiscount
-	);
 }
